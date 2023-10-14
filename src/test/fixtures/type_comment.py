13b383c74c48abedccd6965f381c33c765bc571f
Ava Silver <ava.silver@datadoghq.com>
2023-10-14 14:31:13 -0400

    added more tests

---
+++ src/test/fixtures/type_comment.py
@@ -0,0 +1,4 @@
+def some_func(x, y, z):
+    # type: (int, int, str) -> str
+    """Some docstring"""
+    pass
---
+++ src/test/fixtures/type_comment_with_docstring.py
@@ -0,0 +1,12 @@
+def some_func(x, y, z):
+    # type: (int, int, str) -> str
+    """Some docstring
+    
+    Args:
+        x (int): hmm
+        y (int): okay
+        z (str): bye
+    Returns:
+        str: pretty neat
+    """
+    pass
